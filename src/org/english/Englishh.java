9b78f61434c2dba7654740e8bc07daf96f3c0012
sangeevignesh <sangeethajayasankar03@gmail.com>
2022-09-03 14:19:17 +0530

    sample5

--- src/org/english/Englishh.java
+++ src/org/english/Englishh.java
@@ -3,6 +3,10 @@ package org.english;
 public class Englishh {
 	public void englishLanguage() {
 	System.out.println("English");
+	System.out.println("tamil");
+	System.out.println("maths");
+	System.out.println("science");
+
 
 	}
 
